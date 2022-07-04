9f27434de3760083f739f4e5fa30d0321c5b197a
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-07-04 14:07:49 -0300

    activation functions

--- feedforward.py
+++ feedforward.py
@@ -1,11 +1,27 @@
 class FeedForward():
-    def train(self, Inputs=[], Target=0, Epochs=1):
+    def train(self, Inputs=[], Target=0, Epochs=1, activation='sigmoid'):
+        import math as m
+        self.m = m
+        self.activation = activation
         self.Inputs = Inputs
         self.Target = Target
         self.Epochs = Epochs
 
     def gradientDescent(self, n=0):
         return n * (1 - n)
+
+    # hyperbolic
+    def tanh(self, n=0): return self.m.sinh(n) / self.m.cosh(n)
+    # sigmoid
+    def sigmoid(self, n=0): return 1 / (1 + pow(self.m.e, -n))
+    # relu
+    def relu(self, n=0): return max([n, 0])
+    # leaky relu
+    def leakyRelu(self, n=0): return max([n, .01])
+    # biary
+    def binaryStep(self, n=0):
+        if n >= 0: return 1
+        else: return 0
     
     def predict(self):
         if self.Target <= 0:
@@ -28,7 +44,18 @@ class FeedForward():
                 multiply.append(self.Inputs[j] * weights[j])
                 j += 1
             _sum = sum(multiply)
-            output = round(tanh(_sum), 4)
+            if self.activation == 'tanh':
+                output = round(self.tanh(_sum), 4)
+            elif self.activation == 'sigmoid':
+                output = round(self.sigmoid(_sum), 4)
+            elif self.activation == 'relu':
+                output = round(self.relu(_sum), 4)
+            elif self.activation == 'leakyRelu':
+                output = round(self.leakyRelu(_sum), 4)
+            elif self.activation == 'binaryStep':
+                output = round(self.binaryStep(_sum), 4)
+            else:
+                output = round(self.sigmoid(_sum), 4)
 
             error = abs(self.Target - output)
             j = 0
@@ -37,5 +64,5 @@ class FeedForward():
                 j += 1
             epoch = str(i).rjust(7, '0')
 
-            print(f'period: {epoch} - error tx: {error:.4f} - out: {output:.4f}')
+            print(f'period: {epoch} - error tax: {error:.4f} - output: {output:.4f}')
             i += 1
